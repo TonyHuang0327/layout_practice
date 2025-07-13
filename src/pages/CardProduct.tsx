aee86a04d404864f0c34d090a42782f764017e28
Tony <Tony23046176@gmail.com>
2025-07-13 15:33:36 +0800

    day3 card product

--- src/pages/CardProduct.tsx
+++ src/pages/CardProduct.tsx
@@ -1,40 +1,80 @@
-import React from 'react'
-import '../style/cardproduct.css'
+import "../style/cardproduct.css";
 
-import {
-    Box,
-    Paper, Typography, Button
-} from '@mui/material'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import product from '../assets/teclado-mecanico-gamer-montech-mkey-pro-75-fb-rgb-wireless-bluetooth-switch-gateron-brown-ansi-65-azul-e-branco_217440.jpg'
+import { Box, Paper, Typography, Button, Chip } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import product from "../assets/teclado-mecanico-gamer-montech-mkey-pro-75-fb-rgb-wireless-bluetooth-switch-gateron-brown-ansi-65-azul-e-branco_217440.jpg";
+export const Product = () => {
+  return(
+    <Paper
+        sx={{
+          height: "fit-content",
+          width: "100%",
+          backgroundColor: "white",
+          borderRadius: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding:'10px'
+        }}
+      >
+        <img className="product-pic" src={product} alt="product picture" />
+        <Box sx={{
+          display:'flex',
+          width:'100%',
+          gap:0.5
+        }}>
+        <img className="variety" src={product} alt="product picture" />
+        <img className="variety" src={product} alt="product picture" />
+        <img className="variety" src={product} alt="product picture" />
+        <img className="variety" src={product} alt="product picture" />
+        <img className="variety" src={product} alt="product picture" />
+        </Box>
+        <Box sx={{
+          alignSelf:'flex-start',
+          marginTop:'20px'
+        }}>
+          <Typography variant="body1" color="initial"sx={{ fontSize:'17px', fontWeight:'bold'}}>
+            Montech Mkey Pro
+          </Typography>
+          <Chip label='75% keyboards' sx={{backgroundColor:'#FEF8F2', color:'#E57219'}}/>
+          <Box sx={{
+            display:'flex',
+            gap:1,
+            marginTop:'20px'
+          }}>
+          <Typography variant="body1" color="initial">
+            $139.99
+          </Typography>
+          <Typography variant="body1" color="#AEB9C3" sx={{textDecoration:'line-through'}}>
+            $169.99
+          </Typography>
+          </Box>
+        </Box>
+          <Button variant="contained" color="primary" sx={{ marginTop:'20px', backgroundColor:'#1E293B', width:'100%', borderRadius:'10px'}}>
+            <ShoppingCartIcon />
+            ADD TO CART
+        </Button>
+      </Paper>
+  )
+}
 
 export const CardProduct = () => {
   return (
-    <Box sx={{
-        display:'grid',
-        height:'90vh',
-    }}>
-        <Paper sx={{
-            height:'40%',
-            width:'40%',
-            backgroundColor:'primary',
-            borderRadius:'10px',
-            display:'flex',
-            flexDirection:'column',
-            alignItems:'center',
-            paddingTop:'10px'
-        }}>
-            <img className='product-pic' src={product} alt="product picture" />
-            <Box>
-                <Typography variant="body1" color="initial">Montech Mkey Pro</Typography>
-                <Typography variant="body1" color="initial">Women Shoes</Typography>
-                <Typography variant="body1" color="initial">$139.99</Typography>
-            </Box>
-            <Button variant="contained" color="primary">
-                <ShoppingCartIcon />
-              ADD TO CART
-            </Button>
-        </Paper>
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns:'repeat(3,1fr)',
+        width:'70%',
+        gap:8,
+        margin: "auto",
+      }}
+    >
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
     </Box>
-  )
-}
+  );
+};
